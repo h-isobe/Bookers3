f2f35d62133d57fbe2b7d810dc8145c04f59db1d
磯部浩樹 <isobehiroki@MacBook-Pro.local>
2020-08-27 18:30:06 +0900

    [Add]ページャ機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).reverse_order
     @book = Book.new
     @user = current_user
   end
