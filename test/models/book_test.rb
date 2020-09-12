da21715c1f7829ab4b83b537d9bc90092dbd5e69
磯部浩樹 <h.sep23@gmail.com>
2020-09-12 09:43:09 +0900

    [Add]DM機能

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,18 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  Rspec.describe Book, type: :model do
+    context "データが正しく保存される" do
+      before do
+        @book = Book.new
+        @book.title = "こんにちは"
+        @book.body = "今日も晴れです"
+        @book.save
+      end
+      it "全て入力してあるので保存される" do
+        expect(@book).to be_valid
+      end
+    end
+  end
 end
